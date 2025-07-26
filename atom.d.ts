cb8d902f43326225fd9ceebf0419922ac0ade7ab
Andrew Dupont <github@andrewdupont.net>
2025-07-26 15:32:01 -0700

    Attempt to simplify

--- atom.d.ts
+++ atom.d.ts
@@ -1,19 +1,3 @@
 declare module 'atom' {
   export * from '@pulsar-edit/types';
 }
-
-declare module 'atom/autocomplete-plus' {
-  export * from './autocomplete-plus';
-}
-
-declare module 'atom/linter' {
-  export * from './linter';
-}
-
-declare module 'atom/status-bar' {
-  export * from './status-bar';
-}
-
-declare module 'atom/tool-bar' {
-  export * from './tool-bar';
-}
