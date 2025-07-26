c606837d45e7a0ed53b977a1cd0783d6d255ccc6
Andrew Dupont <github@andrewdupont.net>
2025-07-26 15:05:26 -0700

    Nah, I had it right before

--- atom.d.ts
+++ atom.d.ts
@@ -3,17 +3,17 @@ declare module 'atom' {
 }
 
 declare module 'atom/autocomplete-plus' {
-  export * from './autocomplete-plus';
+  export * from '@pulsar-edit/types/autocomplete-plus';
 }
 
 declare module 'atom/linter' {
-  export * from './linter';
+  export * from '@pulsar-edit/types/linter';
 }
 
 declare module 'atom/status-bar' {
-  export * from './status-bar';
+  export * from '@pulsar-edit/types/status-bar';
 }
 
 declare module 'atom/tool-bar' {
-  export * from './tool-bar';
+  export * from '@pulsar-edit/types/tool-bar';
 }
