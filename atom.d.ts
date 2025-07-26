dce1ad15a2ff9fddb3ff3a21023f4c3ba766abe4
Andrew Dupont <github@andrewdupont.net>
2025-07-26 14:55:15 -0700

    Further fixes

--- atom.d.ts
+++ atom.d.ts
@@ -1,3 +1,19 @@
 declare module 'atom' {
   export * from '@pulsar-edit/types';
 }
+
+declare module 'atom/autocomplete-plus' {
+  export * from '@pulsar-edit/types/autocomplete-plus';
+}
+
+declare module 'atom/linter' {
+  export * from '@pulsar-edit/types/linter';
+}
+
+declare module 'atom/status-bar' {
+  export * from '@pulsar-edit/types/status-bar';
+}
+
+declare module 'atom/tool-bar' {
+  export * from '@pulsar-edit/types/tool-bar';
+}
