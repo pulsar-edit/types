3007e0bd626a4ab2717c97a526dee6cb6448034f
Andrew Dupont <github@andrewdupont.net>
2025-08-17 23:32:55 -0700

    Update the return value of `getLoadSettings`.

--- src/get-window-load-settings.d.ts
+++ src/get-window-load-settings.d.ts
@@ -1,9 +1,26 @@
+type LocationToOpen = {
+  exists: boolean;
+  hasWaitSession: boolean;
+  initialColumn: number | null;
+  initialLine: number | null;
+  isDirectory: boolean;
+  isFile: boolean;
+  pathToOpen: string | null;
+};
+
 export interface WindowLoadSettings {
+  readonly appName: string;
   readonly appVersion: string;
   readonly atomHome: string;
+  readonly clearWindowState: boolean;
   readonly devMode: boolean;
+  readonly env?: { [key: string]: string | undefined } | undefined;
+  readonly hasOpenFiles: boolean;
+  readonly initialProjectRoots: string[];
+  readonly locationsToOpen: LocationToOpen[];
   readonly resourcePath: string;
   readonly safeMode: boolean;
-  readonly env?: { [key: string]: string | undefined } | undefined;
   readonly profileStartup?: boolean | undefined;
+  readonly userSettings: object;
+  readonly windowInitializationScript: string;
 }
